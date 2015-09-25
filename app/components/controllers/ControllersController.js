7427405cf11f44d32170f3d3b7c4b0e625d3bbea
Jonathan Porter <myctrring@gmail.com>
2015-09-24 18:20:22 -0600

    controllers and expressions finished

--- app/components/controllers/ControllersController.js
+++ app/components/controllers/ControllersController.js
@@ -5,4 +5,11 @@ app.controller('ControllersController', ControllersController);
 
 function ControllersController(){
 	this.controllersStatus = 'Working';
+	this.friends = ['Joey', 'Chandler', 'Monica', 'Phebes', 'Ross & Rachel'];
+	this.newFriend = "";
+	this.addFriend = function () {
+		this.friends.push(this.newFriend);
+		this.newFriend = '';
+	}
+	
 }
--- app/components/expressions/ExpressionsController.js
+++ app/components/expressions/ExpressionsController.js
@@ -5,4 +5,11 @@ app.controller('ExpressionsController', ExpressionsController);
 
 function ExpressionsController(){
 	this.expressionsStatus = 'Working';
+	this.test = 'Yipee! It works!'
+}
+
+app.controller('MyOwnFrikinController', MyOwnFrikinController);
+
+function MyOwnFrikinController(){
+	this.isTheBomb = "This is my own frikin property!";
 }
